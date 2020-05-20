9a34a4f57a6f1f1986555a09850632b7330a1f09
Praveen Raj <prvn.raj@gmail.com>
2020-05-20 16:45:08 +0530

    Maths Tables app(without seek bar function)

--- app/src/main/java/com/example/listviewmathtables/MainActivity.java
+++ app/src/main/java/com/example/listviewmathtables/MainActivity.java
@@ -3,12 +3,55 @@ package com.example.listviewmathtables;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.Arrays;
+import java.util.Collections;
 
 public class MainActivity extends AppCompatActivity {
+    //int x;
+   // EditText number = (EditText) findViewById(R.id.editText2);
+   // String S = number.getText().toString();
+    String[] array = {"1","2","3","4","5","6","7","8","9","10"};
+
+    //String array = Arrays.toString(arr);
+        public void Table(View view) {
+            int[] arr = {1,2,3,4,5,6,7,8,9,10};
+            ListView table = findViewById(R.id.listView1);
+            EditText number = (EditText) findViewById(R.id.editText2);
+        String S = number.getText().toString();
+        int x = Integer.parseInt(S);
+
+
+        //int[] arr = {1,2,3,4,5,6,7,8,9,10};
+        for (int i =0; i<arr.length; i++)
+        {
+            arr[i]=arr[i]*x;
+        }
+       for(int i=0; i<arr.length; i++)
+            array[i]=String.valueOf(arr[i]);
+           ArrayAdapter <String> myAdaptor = new ArrayAdapter <String> (this,android.R.layout.simple_list_item_1, array);
+           table.setAdapter(myAdaptor);
+
+        Toast.makeText(this, Arrays.toString(arr), Toast.LENGTH_SHORT).show();
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //ArrayAdapter <String> myAdaptor = new ArrayAdapter <String> (this,android.R.layout.simple_list_item_1, Collections.singletonList(array));
+        //ListView table = findViewById(R.id.listView1);
+       // ArrayAdapter <String> myAdaptor = new ArrayAdapter <String> (this,android.R.layout.simple_list_item_1, array);
+       // table.setAdapter(myAdaptor);
+
+
+
+
     }
 }
